5f76a2e10c0b0fef6238eeb0ea179cf8f1836e8a
roshannizar <roshannizar8@gmail.com>
2019-06-18 19:31:47 +0530

    Node Module Update

---
+++ client/src/component/home/AHome.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import '../dashboard/Dstyle.css';
+
+class AHome extends Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default AHome; 
