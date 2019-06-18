765014f983013ef5157ca797fcdacea226110ad2
roshannizar <roshannizar8@gmail.com>
2019-06-18 20:55:08 +0530

    Reducer index.js file updated

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
+import courseReducer from './courseReducer';
 
 export default combineReducers({
     auth: authReducer,
-    errors: errorReducer
+    errors: errorReducer,
+    course: courseReducer
 });
