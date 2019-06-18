cda189cf1ef8e55145271d3a983413b0aafefcee
roshannizar <roshannizar8@gmail.com>
2019-06-18 21:31:24 +0530

    Prepared for deploy test

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://admin:admin@cluster0-iiynf.azure.mongodb.net/test?retryWrites=true',
-    secretOrKey: 'secret'
-};
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
+}
--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const courses = require('./routes/api/courses');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const db = require('./config/keys').mongoURI;
@@ -21,6 +22,15 @@ app.use(passport.initialize());
 
 app.use('/api/users', users);
 app.use('/api/courses', courses);
+
+if (process.env.NODE_ENV === 'production') {
+
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
