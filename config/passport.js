dd9b518ef4f8f76ac7b34ba217d7914722828cd0
roshannizar <roshannizar8@gmail.com>
2019-06-19 10:45:23 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
-const User = require('users');
+const User = mongoose.model('users');
 const keys = require('../config/keys');
 
 const opts = {};
